228f2270e23ecb89eef070be57caf569e190a3fa
Ryan <ryanrigato@gmail.com>
2020-02-02 12:16:38 -0600

    Outline of test script

---
+++ tests/test_dev_aws_resources.py
@@ -0,0 +1,130 @@
+from bs4 import BeautifulSoup
+import argparse
+import boto3
+import json
+import logging
+import os
+import pandas as pd
+import requests
+import unittest
+
+ENVIRON_DEF = "dev"
+
+HOMEPAGE_URL = 'http://dev-sneakpeek.s3-website-us-east-1.amazonaws.com/'
+WORKING_DIRECTORY = os.getcwd()
+
+def get_logger():
+    '''Returns a boto cloudformation describe_stacks api call
+        Parameters
+        ----------
+        stack_name: str
+            Name of the stack
+
+        Returns
+        -------
+        cf_response : dict
+                Dictionary output of the describe_stacks api call
+
+        Raises
+        ------
+    '''
+    """
+        Adds the file name to the logs/ directory without
+        the extension
+    """
+    logging.basicConfig(
+        filename=os.path.join(WORKING_DIRECTORY, 'logs/',
+        os.path.basename(__file__).split('.')[0]),
+        format='%(asctime)s %(message)s',
+         datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG
+         )
+    logging.info('\n')
+
+def get_boto_clients(resource_name, region_name='us-east-1'):
+    '''Returns the boto client for various cloudformation resources
+        Parameters
+        ----------
+        resource_name : str
+            Name of the resource for the client
+
+        region_name : str
+                aws region you are using, defaults to
+                us-east-1
+
+        Returns
+        -------
+
+
+        Raises
+        ------
+    '''
+    return(boto3.client(resource_name, region_name))
+
+
+class WebappLive(unittest.TestCase):
+    '''Tests that the aws resources necessary for the webpage are running
+
+        Note that if any of the below unit tests fail,
+        The python script will have a non-zero exit code
+
+        This will cause any CodeBuild Builds to fail out
+
+        Preventing the Code Pipeline from continuing to delivery
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    '''
+    @classmethod
+    def setUpClass(self):
+        '''Unitest function that is run once for the class
+            Gets the arguements passed from the user
+
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        '''
+        get_logger()
+
+    def test_home_page(self):
+        '''Tests that the aws resources necessary for the webpage are running
+
+            Parameters
+            ----------
+                request_url : str
+                    Url string to send the request to
+            Returns
+            -------
+
+            Raises
+            ------
+        '''
+        logging.info("Testing if the website is alive")
+        r = requests.get(
+            HOMEPAGE_URL
+        )
+        self.assertEqual(r.status_code, 200)
+        logging.info("The website is live")
+
+
+
+if __name__ == '__main__':
+    '''
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                        help='an integer for the accumulator')
+    args = parser.parse_args()
+
+    print(args.integers)
+    '''
+    unittest.main()
