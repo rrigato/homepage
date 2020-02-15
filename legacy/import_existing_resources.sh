a29dd8c0dd15aa9455e1f52e55040db0513decef
Ryan <ryanrigato@gmail.com>
2020-02-15 11:46:38 -0600

    First pass at importing existing s3 resource

--- legacy/import_existing_resources.sh
+++ legacy/import_existing_resources.sh
@@ -4,3 +4,16 @@
 #cloudformation import
 aws cloudformation get-template-summary \
     --template-body file://legacy/resources/legacy_resources.yml
+
+##########################
+#ResourceType = s3 bucket
+#LogicalResourceId = what you called the resource in
+#the cloudformation template you are importing
+#ResourceIdentifier = key of existing resource, for s3
+# the bucket name
+##########################
+aws cloudformation create-change-set \
+    --stack-name prod-homepage --change-set-name S3ImportChangeSet \
+    --change-set-type IMPORT \
+    --resources-to-import "[{\"ResourceType\":\"AWS::S3::Bucket\",\"LogicalResourceId\":\"WebsiteBucket\",\"ResourceIdentifier\":{\"BucketName\":\"ryanrigato.com\"}}]" \
+    --template-body file://templateToImport.json
