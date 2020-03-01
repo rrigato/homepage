36cba18e7c0da150761803f05f661371abbc6e08
Ryan <ryanrigato@gmail.com>
2020-03-01 16:05:10 -0600

    Working on Route53 hosted zone import

--- legacy/import_existing_resources.sh
+++ legacy/import_existing_resources.sh
@@ -24,3 +24,18 @@ aws cloudformation describe-change-set \
 
 aws cloudformation execute-change-set \
 --change-set-name S3ImportChangeSet --stack-name prod-homepage
+
+
+
+##########################
+#ResourceType = Route53 HostedZone
+#LogicalResourceId = what you called the resource in
+#the cloudformation template you are importing
+#ResourceIdentifier = key of existing resource,
+#HostedZoneId
+##########################
+aws cloudformation create-change-set \
+    --stack-name prod-homepage --change-set-name Route53ImportChangeSet \
+    --change-set-type IMPORT \
+    --resources-to-import file://legacy/resources/route53_resources_to_import.txt \
+    --template-body file://templates/prod_resource_import.yml
