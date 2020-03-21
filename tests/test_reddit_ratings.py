15c20d302401fab5c1b0596db3e6e44acb3b3502
Ryan <ryanrigato@gmail.com>
2020-03-21 18:08:28 -0500

    Test script initialization for reddit oath

---
+++ tests/test_reddit_ratings.py
@@ -0,0 +1,121 @@
+from bs4 import BeautifulSoup
+from unittest.mock import MagicMock
+from unittest.mock import patch
+import json
+import logging
+import os
+import requests
+import unittest
+
+'''
+    Defaults to $CODEBUILD_SRC_DIR
+    which is the working directory where the
+    code repository is built in codebuild
+'''
+WORKING_DIRECTORY = os.getcwd()
+
+def get_logger():
+    """Returns a boto cloudformation describe_stacks api call
+        Parameters
+        ----------
+        stack_name: str
+            Name of the stack
+
+        Returns
+        -------
+        cf_response : dict
+                Dictionary output of the describe_stacks api call
+
+        Raises
+        ------
+    """
+    '''
+        Adds the file name to the logs/ directory without
+        the extension
+    '''
+    logging.basicConfig(
+        filename=os.path.join(WORKING_DIRECTORY, "logs/",
+        os.path.basename(__file__).split(".")[0]),
+        format="%(asctime)s %(message)s",
+         datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.DEBUG
+         )
+    logging.info("\n")
+
+class WebappLive(unittest.TestCase):
+    """Tests that the aws resources necessary for the webpage are running
+
+        Note that if any of the below unit tests fail,
+        The python script will have a non-zero exit code
+
+        This will cause any CodeBuild Builds to fail out
+
+        Preventing the Code Pipeline from continuing to delivery
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+        Raises
+        ------
+    """
+    @classmethod
+    def setUpClass(self):
+        """Unitest function that is run once for the class
+
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        """
+        get_logger()
+        os.sys.path.append(WORKING_DIRECTORY)
+
+    def setUp(self):
+        """Unitest function that is run once before each function
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        """
+        os.chdir(WORKING_DIRECTORY)
+
+    @unittest.skip("Skipping for now")
+    def test_oath_key(self):
+        """Tests oath key returned from reddit api
+
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        """
+        logging.info("Testing if the website bucket stack exists")
+
+
+        webpage_stack = describe_stacks_response(
+            stack_name=stack_name)
+
+        self.assertEqual(
+            webpage_stack["Outputs"][0]["OutputValue"],
+            HOMEPAGE_URL
+        )
+        logging.info("Output url for webpage is correct")
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
