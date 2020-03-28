6b343603fdfd0c323ab24cf07942b8098414c424
Ryan <ryanrigato@gmail.com>
2020-03-28 17:08:05 -0500

    Url_param_dict added

--- scripts/reddit_ratings.py
+++ scripts/reddit_ratings.py
@@ -96,11 +96,30 @@ def get_news_flair(access_token, fullname_after):
         -------
         news_flair_posts : dict
             Dict of all posts after fullname_after
-            
+
 
         Raises
         ------
     """
+    '''
+        API Query Parameter explanation
+        q=flair:news
+            Searching posts with a flair of new
+        limit=10
+            limit of how many posts to return
+        sort=new
+            sort by new posts
+        restrict_sr=on
+            restrict subreddit on to only search /r/toonami
+        t=all
+            time period, all
+        raw_json=1
+            Converts &lt; &gt; and &amp;
+            to < > and & in response body
+    '''    
+    url_param_dict = {
+
+    }
 
 def handle_table_header(bs_obj):
     """Converts table header for the html table into list
@@ -207,22 +226,6 @@ def main():
     get_logger()
     REDDIT_CLIENT_KEY = os.environ.get("REDDIT_CLIENT_KEY")
     REDDIT_CLIENT_SECRET = os.environ.get("REDDIT_CLIENT_SECRET")
-    '''
-        API Query Parameter explanation
-        q=flair:news
-            Searching posts with a flair of new
-        limit=10
-            limit of how many posts to return
-        sort=new
-            sort by new posts
-        restrict_sr=on
-            restrict subreddit on to only search /r/toonami
-        t=all
-            time period, all
-        raw_json=1
-            Converts &lt; &gt; and &amp;
-            to < > and & in response body
-    '''
     #html_table_parse("""<!-- SC_OFF --><div class=\"md\"><table><thead>\n<tr>\n<th align=\"center\">Time</th>\n<th align=\"center\">Show</th>\n<th align=\"center\">Viewers (000)</th>\n<th align=\"center\">18-49 Rating</th>\n<th align=\"center\">18-49 Views (000)</th>\n</tr>\n</thead><tbody>\n<tr>\n<td align=\"center\">11:30</td>\n<td align=\"center\">My Hero Academia</td>\n<td align=\"center\">543</td>\n<td align=\"center\">0.26</td>\n<td align=\"center\">332</td>\n</tr>\n<tr>\n<td align=\"center\">12:00a</td>\n<td align=\"center\">Sword Art Online: Alicization - War of Underworld</td>\n<td align=\"center\">385</td>\n<td align=\"center\">0.19</td>\n<td align=\"center\">245</td>\n</tr>\n<tr>\n<td align=\"center\">12:30a</td>\n<td align=\"center\">Demon Slayer</td>\n<td align=\"center\">358</td>\n<td align=\"center\">0.18</td>\n<td align=\"center\">232</td>\n</tr>\n<tr>\n<td align=\"center\">1:00a</td>\n<td align=\"center\">Food Wars!</td>\n<td align=\"center\">306</td>\n<td align=\"center\">0.16</td>\n<td align=\"center\">207</td>\n</tr>\n<tr>\n<td align=\"center\">1:30a</td>\n<td align=\"center\">Black Clover</td>\n<td align=\"center\">275</td>\n<td align=\"center\">0.15</td>\n<td align=\"center\">196</td>\n</tr>\n<tr>\n<td align=\"center\">2:00a</td>\n<td align=\"center\">Jojo’s Bizarre Adventure: Golden Wind</td>\n<td align=\"center\">235</td>\n<td align=\"center\">0.13</td>\n<td align=\"center\">170</td>\n</tr>\n<tr>\n<td align=\"center\">2:30a</td>\n<td align=\"center\">Naruto: Shippuden</td>\n<td align=\"center\">236</td>\n<td align=\"center\">0.13</td>\n<td align=\"center\">170</td>\n</tr>\n</tbody></table>\n\n<p>Source: <a href=\"https://programminginsider.com/saturday-final-ratings-hbo-premiere-of-hobbs-shaw-beats-nbc-telecast-of-its-parent-2017-film-the-fate-of-the-furious-among-adults-18-49/\">https://programminginsider.com/saturday-final-ratings-hbo-premiere-of-hobbs-shaw-beats-nbc-telecast-of-its-parent-2017-film-the-fate-of-the-furious-among-adults-18-49/</a></p>\n</div><!-- SC_ON -->""")
 
 if __name__ == "__main__":
