1b851d7f83b9cdc7021705d44d98c9e4b32d4d85
Ryan <ryanrigato@gmail.com>
2020-05-25 09:58:35 -0500

    stub for get_boto_client

--- scripts/reddit_ratings.py
+++ scripts/reddit_ratings.py
@@ -41,6 +41,29 @@ def get_logger(working_directory=os.getcwd()):
          )
     logging.info("\n")
 
+def get_boto_clients(resource_name, region_name='us-east-1'):
+    '''Returns the boto client for various aws resources
+
+        Parameters
+        ----------
+        resource_name : str
+            Name of the resource for the client
+
+        region_name : str
+                aws region you are using, defaults to
+                us-east-1
+
+        Returns
+        -------
+
+
+        Raises
+        ------
+    '''
+    return(boto3.client(resource_name, region_name))
+
+
+
 def get_client_secrets(self):
     """Returns reddit client key and reddit client secret 
 
