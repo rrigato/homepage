2738c75506717882e9e76d2ffffd801f7c461592
rrigato <ryanrigato@gmail.com>
2023-09-03 16:14:22 -0500

    react initial setup

---
+++ static/App.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client';
+
+
+
+const root = createRoot(
+    document.getElementById('app-entry-point')
+);
+root.render(<h1>react initial setup</h1>);
---
+++ static/tests/App.test.js
@@ -0,0 +1,5 @@
+describe('entry point for app', () => {
+    test('', () => {
+        expect(sum(1, 2)).toBe(3);
+    });
+});
