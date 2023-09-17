620c12a5cf610fa95210e3658b0a6b130260c6d8
rrigato <ryanrigato@gmail.com>
2023-09-17 12:28:54 -0500

    correct directory

--- static/webpack.local.js
+++ static/webpack.local.js
@@ -3,7 +3,14 @@ const baseConfig = require('./webpack.config.js');
 
 module.exports = merge(
     baseConfig,
+    
     {
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist'),
+            },
+            open: true
+        },
         mode: 'development'
     }
 );
