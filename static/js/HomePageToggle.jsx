4cdd8cd7e8b83d3200ed143ec198adb436b6c98d
rrigato <ryanrigato@gmail.com>
2023-11-11 10:20:39 -0600

    Projects button

--- static/js/HomePageToggle.jsx
+++ static/js/HomePageToggle.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
+import {useState} from 'react';
 import '../css/main.css';
 
 export function HomePageToggle(){
+    /**
+     * @type {[Number, Function]}
+     */
+    const [selectedSection, setSelectedSelection] = useState(0);
     return(
         <div id='home-page-toggle-component'  className='full-height'>
             <div className='toggle-container'>
-                <button>Projects</button>
+                <button
+                    // onClick={
+                    //     setSelectedSelection(0)
+                    // }
+                >Projects
+                </button>
+                {/* <button
+                    onClick={
+                        setSelectedSelection(1)
+                    }
+                >About</button>
+                <button
+                    onClick={
+                        setSelectedSelection(2)
+                    }
+                >Blog</button> */}
                 <br/>
-                <div id='toggle-0'>
+                <div 
+                    id='toggle-0' 
+                    hidden= {
+                        selectedSection !== 0
+                    }>
                     Latest Projects
                 </div>
             </div>
