777a8141956ef8e8c861e47848abe7fe5405da84
rrigato <ryanrigato@gmail.com>
2023-11-11 09:47:43 -0600

    flexbox to center page

--- static/js/HomePageToggle.jsx
+++ static/js/HomePageToggle.jsx
@@ -3,10 +3,13 @@ import '../css/main.css';
 
 export function HomePageToggle(){
     return(
-        <div id='home-page-toggle-component' className='toggle-container'>
+        <div id='home-page-toggle-component'  className='full-height'>
+        <div 
+            className='toggle-container'>
             <div id='toggle-0'>
                 Latest Projects
             </div>
+        </div>
         </div>
     );
 }
