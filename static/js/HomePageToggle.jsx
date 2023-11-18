f19b1a2828d149b718dff548816582304ac04fd9
rrigato <ryanrigato@gmail.com>
2023-11-18 10:29:04 -0600

    initial projects

--- static/js/HomePageToggle.jsx
+++ static/js/HomePageToggle.jsx
@@ -76,7 +76,31 @@ export function HomePageToggle(){
                     hidden= {
                         selectedSection !== 0
                     }>
-                    Latest Projects
+                    <div className='homepage-content'>
+                        <ul>
+                            <h3>Amazon Alexa Skill To retrieve Television Ratings</h3>
+                            <li>Provides television ratings for the Adult Swim Saturday night Toonami television block.</li>
+                            <li><a href='https://www.amazon.com/dp/B0B5596H7C/'>Application link</a></li>
+                            <li><a href='https://github.com/rrigato/tvratings'>Source Code</a></li>
+
+                            <h3>Amazon Alexa Air Quality Burning Restriction Skill</h3>
+                            <li>Amazon Alexa skill to determine if a zip code has any fuel burning restrictions for heating your home in the winter.</li>
+                            <li><a href='https://www.amazon.com/dp/B09PVD9VSC/'>Application link</a></li>
+                            <li><a href='https://github.com/rrigato/burnday'>Source Code</a></li>
+
+                            <h3>Web scrap television network ratings</h3>
+                            <li>Only location on the internet that has an archive of Adult Swim Toonami Ratings in a programmable format.</li>
+                            <li>Reach out to me on github if you would like access.</li>
+                            <li><sup><sup>First rule of building a sustainable web scraping application is to never build a web scrapping application.</sup></sup></li>
+                            <li><a href='https://github.com/rrigato/ratings'>Source Code</a></li>
+
+                            <h3>Github 3d commit history webpage</h3>
+                            <li>Automates creation of 3D commit history to populate web application artifacts</li>
+                            <li><a href='https://github.com/rrigato/rrigato/blob/master/README.md'>Application link</a></li>
+                            <li><a href='https://github.com/rrigato/rrigato/'>Source Code</a></li>
+                        </ul>
+
+                    </div>
                 </div>
                 <div 
                     id='toggle-1' 
