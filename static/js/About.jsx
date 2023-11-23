2bb04f9ac9d092c6e82aaaac9b74ad30566f4f22
rrigato <ryanrigato@gmail.com>
2023-11-23 13:20:52 -0600

    About page component

---
+++ static/js/About.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import '../css/homepageSection.css';
+
+
+/**Details about the website
+ * 
+ * @returns react jsx 
+ */
+export function About(){
+    return(
+        <div className='homepage-content'>
+            <h3>What do I need to know about this website?</h3>
+            <div>
+                <ul>
+                    <li>Anytime I look back at an application I built more than a year I am embarrased by the quality of the architecture. This is the trademark of a good engineer because it means they are always improving their kraft.</li>
+                    <li>This webiste went from a Java EE2 web app hosted in google app engine all the way to serverless web app deployed to AWS...with many migration tears in between!</li>
+                    <li>The world's largest tuned mass damper is in the 
+                        <a href='https://en.wikipedia.org/wiki/Tuned_mass_damper'>Taipei 101 tower</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
---
+++ static/tests/About.test.js
@@ -0,0 +1,20 @@
+import {  render } from '@testing-library/react';
+import { About } from '../js/About.jsx';
+
+
+describe('About displayed on screen', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+      
+    test('About section', async () => {        
+        
+
+        const {getAllByRole} = render(<About/>);
+
+        
+        const numProjectHeaders = getAllByRole(
+            'heading'
+        );
+    });
+});
