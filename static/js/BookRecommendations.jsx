704307249bf4728f50e231511c65d4da5ffebb79
rrigato <ryanrigato@gmail.com>
2023-11-25 08:51:26 -0600

    Stub out BookRecommendations component

---
+++ static/js/BookRecommendations.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/homepageSection.css';
+import aboutPhoto from '../img/aboutPhoto.jpg';
+
+/**Book Recommendations
+ * 
+ * @returns react jsx 
+ */
+export function BookRecommendations(){
+    return(
+        <div className='homepage-content'>
+            <div>
+                <ul>
+                    <h3>Everyday Reads</h3>
+                    <h3>Software Engineering</h3>
+                </ul>
+            </div>
+        </div>
+    );
+}
