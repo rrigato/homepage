ce05416aabcac93bb3aada85cc2b1f92906bfc23
rrigato <ryanrigato@gmail.com>
2023-11-26 10:10:07 -0600

    Open link in new page

--- static/js/BookRecommendations.jsx
+++ static/js/BookRecommendations.jsx
@@ -12,7 +12,10 @@ export function BookRecommendations(){
                 <ul>
                     <h3>Everyday Reads</h3>
                     <li>Incerto by
-                        <a href='https://en.wikipedia.org/wiki/Nassim_Nicholas_Taleb#Writing_career'>Nassim Taleb</a>
+                        <a 
+                            href='https://en.wikipedia.org/wiki/Nassim_Nicholas_Taleb#Writing_career'
+                            target={'_blank'}
+                        >Nassim Taleb</a>
 
                     </li>
                     <li className='book-details'>
@@ -20,7 +23,9 @@ export function BookRecommendations(){
                     </li>
 
                     <li>Outlive by
-                        <a href='https://www.amazon.com/Life-Changing-Magic-Tidying-Decluttering-Organizing/dp/1607747308'
+                        <a 
+                            href='https://www.amazon.com/Life-Changing-Magic-Tidying-Decluttering-Organizing/dp/1607747308'                        
+                            target={'_blank'}
                         >Peter Attia</a>
 
                     </li>
@@ -29,7 +34,9 @@ export function BookRecommendations(){
                     </li>
 
                     <li>The Life cleaning magic of tidying up by
-                        <a href='https://www.amazon.com/Life-Changing-Magic-Tidying-Decluttering-Organizing/dp/1607747308'
+                        <a 
+                            href='https://www.amazon.com/Life-Changing-Magic-Tidying-Decluttering-Organizing/dp/1607747308'                        
+                            target={'_blank'}
                         >Marie Kondo</a>
 
                     </li>
@@ -38,7 +45,9 @@ export function BookRecommendations(){
                     </li>
 
                     <li>The power of bad by
-                        <a href='https://www.amazon.com/Power-Bad-Negativity-Effect-Rules-ebook/dp/B07Q3NHPGZ'
+                        <a 
+                            href='https://www.amazon.com/Power-Bad-Negativity-Effect-Rules-ebook/dp/B07Q3NHPGZ'                        
+                            target={'_blank'}
                         > John Tierney and Roy Baumeister</a>
 
                     </li>
@@ -50,14 +59,19 @@ export function BookRecommendations(){
                     <h3>Software Engineering</h3>
 
                     <li>Clean Architecture by
-                        <a href='https://www.amazon.com/Clean-Architecture-Craftsmans-Software-Structure/dp/0134494164'>Bob Martin</a>
+                        <a 
+                            href='https://www.amazon.com/Clean-Architecture-Craftsmans-Software-Structure/dp/0134494164'
+                            target={'_blank'}
+                        >Bob Martin</a>
                     </li>
                     <li className='book-details'>
                         The goal of software architecture is to minimize the human resources required to build and maintain the desired application.
                     </li>
 
                     <li>Rework by
-                        <a href='https://basecamp.com/books/rework'
+                        <a 
+                            href='https://basecamp.com/books/rework'                        
+                            target={'_blank'}
                         >DHH and Jason Fried</a>
                     </li>
                     <li className='book-details'>
@@ -65,11 +79,24 @@ export function BookRecommendations(){
                     </li>
 
                     <li>Shape Up by
-                        <a href='https://basecamp.com/shapeup'>Ryan Singer</a>
+                        <a 
+                            href='https://basecamp.com/shapeup'
+                            target={'_blank'}
+                        >Ryan Singer</a>
                     </li>
                     <li className='book-details'>
                         Outside of the fact that he objectively has the coolest first name a human can have,
                         this is the definitive guide on how iterations should be structured in any devops oriented team.
+                    </li>
+
+                    <li>The Mythical Man Month by
+                        <a 
+                            href='https://en.wikipedia.org/wiki/The_Mythical_Man-Month'                        
+                            target={'_blank'}
+                        >Fred Brooks</a>
+                    </li>
+                    <li className='book-details'>
+                        Scaling software engineering team size leads to increased communication overhead and might make the project later.
                     </li>
 
                 </ul>
